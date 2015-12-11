9533eeafd23669db8d8fee1659fdefe2fcaa261c
Svyatoslav Gryaznov <nightuser@ya.ru>
2015-12-11 21:02:56 +0300

    Fix missing where

--- ClassInheritanceProxified.hs
+++ ClassInheritanceProxified.hs
@@ -9,11 +9,9 @@
 {-# LANGUAGE TypeFamilies              #-}
 {-# LANGUAGE UndecidableInstances      #-}
 
-module ClassInheritanceProxified
- 
-import Unsafe.Coerce
-import Control.Applicative
+module ClassInheritanceProxified where
 
+import Unsafe.Coerce
 
 -- copied from article
 -- http://joyoftypes.blogspot.ru/2012/02/haskell-supports-first-class-instances.html
@@ -30,9 +28,9 @@ unProxyDict = unsafeCoerce
 
 proxyDictFromLabel :: c (Proxy label p) => label -> Dict (c (Proxy label p))
 proxyDictFromLabel _ = Dict
- 
+
 dictFromLabel :: c (Proxy label p) => label -> Dict (c p)
-dictFromLabel a = unProxyDict $ proxyDictFromLabel a 
+dictFromLabel a = unProxyDict $ proxyDictFromLabel a
 
 
 -- proxy supplimentary
@@ -40,7 +38,7 @@ reProxy :: (Proxy l1 p) -> (Proxy l2 p)
 reProxy (Proxy p) = Proxy p
 
 
--- basic tree 
+-- basic tree
 data Tree a = Leaf a | Node (Tree a) (Tree a) deriving Show
 
 
@@ -51,15 +49,15 @@ class TreeNodeHandler res tin | tin -> res where
 class TreeLeafHandler res tin | tin -> res where
     handleLeaf :: tin -> res
 
-class (TreeNodeHandler res (Proxy label tin), TreeLeafHandler res (Proxy label tin)) => 
+class (TreeNodeHandler res (Proxy label tin), TreeLeafHandler res (Proxy label tin)) =>
       CustomizableTreeMap label res tin | label tin -> res where
     mapp :: label -> (Tree tin) -> res
     mapp l (Leaf a) = withDict ((dictFromLabel l) :: Dict (TreeLeafHandler res tin)) $ handleLeaf a
-    mapp l (Node a b) = withDict ((dictFromLabel l) :: Dict (TreeNodeHandler res tin)) $ 
+    mapp l (Node a b) = withDict ((dictFromLabel l) :: Dict (TreeNodeHandler res tin)) $
                                                          handleNode (undefined :: tin) (mapp l a) (mapp l b)
 
 
--- call base method by default 
+-- call base method by default
 instance {-# OVERLAPPABLE #-} (BaseLabel l ~ bl, TreeNodeHandler res (Proxy bl tin)) =>
                               TreeNodeHandler res (Proxy l tin) where
     handleNode _ = handleNode (undefined :: (Proxy bl tin))
@@ -69,25 +67,25 @@ instance {-# OVERLAPPABLE #-} (BaseLabel l ~ bl, TreeLeafHandler res (Proxy bl t
     handleLeaf x = handleLeaf ((reProxy x) :: (Proxy bl tin))
 
 
--- label's stuff 
-type family BaseLabel l 
+-- label's stuff
+type family BaseLabel l
 class LabelWithBase l baseL | l -> baseL
 
 data SummarizeMap = SummarizeMap
 data CloneMap = CloneMap
 
-data SwappyMap = SwappyMap 
+data SwappyMap = SwappyMap
 type instance BaseLabel SwappyMap = CloneMap
 
 data SwappyP1Map = SwappyP1Map
 type instance BaseLabel SwappyP1Map = SwappyMap
- 
+
 instance TreeNodeHandler Int (Proxy SummarizeMap Int) where
     handleNode _ = (+)
 instance TreeLeafHandler Int (Proxy SummarizeMap Int) where
     handleLeaf (Proxy x) = x
 instance CustomizableTreeMap SummarizeMap Int Int
- 
+
 instance TreeNodeHandler (Tree Int) (Proxy CloneMap Int) where
     handleNode _ = Node
 instance TreeLeafHandler (Tree Int) (Proxy CloneMap Int) where
@@ -105,7 +103,7 @@ instance CustomizableTreeMap SwappyP1Map (Tree Int) Int
 
 -- tests
 tree :: Tree Int
-tree = Node (Node (Leaf 8) (Leaf 0)) (Leaf 2)  
+tree = Node (Node (Leaf 8) (Leaf 0)) (Leaf 2)
 
 test1 = mapp SummarizeMap tree    -- >   10
 test2 = mapp SwappyMap    tree       -- >   Node (Leaf 2) (Node (Leaf 0) (Leaf 8))
